ddd826ba637290959f463fc9c8389b4a7c83a791
Caio <81130893+caioalexleme@users.noreply.github.com>
2023-10-18 09:20:54 -0300

    update

---
+++ 02_statements/app.js
@@ -0,0 +1,11 @@
+// início da aplicação
+
+/*
+
+Bloco de comentário
+
+*/
+
+console.clear();
+console.log("A");
+console.log("B");
---
+++ 04_strict/app.js
@@ -0,0 +1,5 @@
+"use strict";
+// Indica que existe erro no código, apesar de funcionar se tirar essa instrução, mas houve atualizações de sixtaxe, nesse caso existe um erro na definição da variável, precisa ter antes o tipo (const, var, let). Usado para forçar a linguagem a ser codificada na forma mais moderna.
+
+nome = "caio";
+console.log(nome);
