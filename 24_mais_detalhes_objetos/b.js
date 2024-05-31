98608e81be45499fc75f602caecabd6d81dc747b
Caio <81130893+caioalexleme@users.noreply.github.com>
2024-05-31 13:00:51 -0300

    24_mais_detalhes_objetos

---
+++ 24_mais_detalhes_objetos/b.js
@@ -0,0 +1,13 @@
+const zonas = {
+    
+    3: 'Escritório',
+    2: 'Fábrica',
+    1: 'Armazém',
+    4: 'Loja'
+}
+
+for (key in zonas) {
+    console.log(key)
+}
+
+//- Perceba caso a chave seja numérica como nesse caso, mesmo desordenado, o output ordena como default
